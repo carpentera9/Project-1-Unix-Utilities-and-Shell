2bbaca02af20377fab9595d0f2f923ba1790e58e
Alec Carpenter <xlcarplx@Alecs-MacBook-Pro-2.local>
2024-10-08 12:45:27 -0400

    HW

---
+++ exec_a_new_program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
+        return 1;
+    }
+
+    execv(argv[1], &argv[1]);
+    perror("execv failed");
+    return 1;
+}
+
